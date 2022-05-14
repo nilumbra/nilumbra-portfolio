6b84c0d16d179a54efa668e5ee50eae5d7557635
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-14 16:09:42 +0900

    checkout:add and tested all content types: about-me, resume, portolio

--- config/database.js
+++ config/database.js
@@ -1,11 +1,30 @@
 const path = require('path');
 
+// const SQLITE_DEFAULT_CONFIG = {
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// }
+
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi_local"),
+      username: env("DATABASE_USERNAME", "strapi_admin"),
+      password: env("DATABASE_PASSWORD", "admin_nilumbra"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-    useNullAsDefault: true,
-  },
+  }
 });
+
+
+
+
