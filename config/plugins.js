8d4a5b1d592eedb68c7ea4ccdbac79c352a99cb0
Mike Chen(HuaXiao) <478222482@qq.com>
2022-05-19 20:42:42 +0900

    add:aws s3 provider for media library

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_KEY'),
+        secretAccessKey: env('AWS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET')
+        },
+      },
+    },
+  },
+  // ...
+});
